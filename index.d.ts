88c953a13a5b7f0bdc550616a22719afc48acb64
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2018-08-06 15:59:26 -0400

    initial commit w/ stubs

---
+++ index.d.ts
@@ -0,0 +1,120 @@
+declare module 'adobe-sign-sdk' {
+    export class OAuthApi {
+    }
+
+    export const AgreementsModel: AgreementsIndex;
+    export const AgreementsApi: AgreementsApi;
+    export const BaseUrisModel: BaseUrisIndex;
+    export const BaseUrisApi: BaseUrisApi;
+    export const GroupsModel: GroupsIndex;
+    export const GroupsApi: GroupsApi;
+    export const LibraryDocumentsModel: LibraryDocumentsIndex;
+    export const LibraryDocumentsApi: LibraryDocumentsApi;
+    export const MegaSignsModel: MegaSignsIndex;
+    export const MegaSignsApi: MegaSignsApi;
+    export const OAuthModel: OAuthIndex;
+    export const RemindersModel: RemindersIndex;
+    export const RemindersApi: RemindersApi;
+    export const SearchModel: SearchIndex;
+    export const SearchApi: SearchApi;
+    export const TransientDocumentsModel: TransientDocumentsIndex;
+    export const TransientDocumentsApi: TransientDocumentsApi;
+    export const UsersModel: UsersIndex;
+    export const UsersApi: UsersApi;
+    export const ViewsModel: ViewsIndex;
+    export const ViewsApi: ViewsApi;
+    export const WidgetsModel: WidgetsIndex;
+    export const WidgetsApi: WidgetsApi;
+    export const WorkflowsModel: WorkflowsIndex;
+    export const WorkflowsApi: WorkflowsApi;
+    export const ApiError: ApiError;
+    export const Context: Context;
+}
+
+interface GroupsIndex {
+}
+
+interface BaseUrisApi {
+}
+
+interface BaseUrisIndex {
+}
+
+interface AgreementsIndex {
+}
+
+interface AgreementsApi {
+}
+
+interface GroupsApi {
+}
+
+interface LibraryDocumentsIndex {
+}
+
+interface GroupsApi {
+}
+
+interface LibraryDocumentsIndex {
+}
+
+interface LibraryDocumentsApi {
+}
+
+interface MegaSignsIndex {
+}
+
+interface MegaSignsApi {
+}
+
+interface OAuthIndex {
+}
+
+interface RemindersIndex {
+}
+
+interface RemindersApi {
+}
+
+interface SearchIndex {
+}
+
+interface SearchApi {
+}
+
+interface TransientDocumentsIndex {
+}
+
+interface TransientDocumentsApi {
+}
+
+interface UsersIndex {
+}
+
+interface UsersApi {
+}
+
+interface ViewsIndex {
+}
+
+interface ViewsApi {
+}
+
+interface WidgetsIndex {
+}
+
+interface WidgetsApi {
+}
+
+interface WorkflowsIndex {
+}
+
+interface WorkflowsApi {
+}
+
+interface ApiError {
+}
+
+interface Context {
+}
+
